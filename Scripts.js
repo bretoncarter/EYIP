153f0bf4c72c44d760d23de361f2f273fd068b3f
Breton <bec2132@barnard.edu>
2015-04-20 17:15:28 -0400

    fix contact sheet

---
+++ Scripts.js
@@ -0,0 +1,9 @@
+Scripts.js
+$(document).ready(function() {
+    $('#bt1').click(function() {
+        $('#fr1').attr('action',
+                       'mailto:test@test.com?subject=' +
+                       $('#tb1').val() + '&body=' + $('#tb2').val());
+        $('#fr1').submit();
+    });
+});
